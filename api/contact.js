341c9e91b3458d514931dcaf191901a25df99c86
Akindakly <akindakly@gmail.com>
2026-07-26 21:52:28 +0200

    Use verified email domain

--- api/contact.js
+++ api/contact.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
         await resend.emails.send({
             from: "Kindakli Elektrotechnik <kontakt@kindakli-elektro.de>",
 
-            to: "kindakli.elektro@gmail.de",
+            to: "kindakli.elektro@gmail.com",
 
             subject: "Neue Kontaktanfrage - Kindakli Elektrotechnik",
 
